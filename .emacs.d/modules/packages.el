c1f3ac6c7f9fbb53e7ad2c21dc36701de6237b6a
V S <vatosarmat@yandex.ru>
2023-06-23 19:18:00 +1000

    emacs non-english keyboard

--- .emacs.d/modules/packages.el
+++ .emacs.d/modules/packages.el
@@ -258,3 +258,21 @@ Note that `line-move' can be very slow.")
 
 (use-package visual-fill-column
   :hook (org-mode . dotfiles/org-mode-visual-fill))
+
+(use-package char-fold
+  :custom
+  (char-fold-symmetric t)
+  (search-default-mode #'char-fold-to-regexp))
+
+(use-package reverse-im
+  :ensure t ; install `reverse-im' using package.el
+  :demand t ; always load it
+  :after char-fold ; but only after `char-fold' is loaded
+  :bind
+  ("M-T" . reverse-im-translate-word) ; fix a word in wrong layout
+  :custom
+  (reverse-im-char-fold t) ; use lax matching
+  (reverse-im-read-char-advice-function #'reverse-im-read-char-include)
+  (reverse-im-input-methods '("russian-computer")) ; translate these methods
+  :config
+  (reverse-im-mode t)) ; turn the mode on
